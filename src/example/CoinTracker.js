b05e0d8dd2e6f039f7e5fa940cab585063af5923
kimmiso <owl6068@naver.com>
2023-10-16 13:25:12 +0900

    코인 갯수 계산 추가 예제 작업

--- src/example/CoinTracker.js
+++ src/example/CoinTracker.js
@@ -4,22 +4,40 @@ import Loading from "./component/Loading"
 function CoinTracker(){
   const [loading, setLoading] = useState(true)
   const [coins, setCoin] = useState([])
+  const [moneys, setMoneys] = useState('')
+
+  const onChange = (e) => {
+    setMoneys(() => {
+      const value = e.target.value;
+      const numValue = value.replaceAll(',', '');
+      return numValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    })
+  }
+  console.log(moneys)
+
   useEffect(()=>{
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then((response) => response.json())
     .then((json) => {
-      setCoin(json.slice(20))
+      setCoin(json.slice(0,10))
       setLoading(false)
     })
   },[])
-  console.log(coins)
   return (
     <div>
       <h1>Coin Tracker</h1><br/>
+      <form>
+        <input value={moneys} type="nuber" onChange={onChange}/>
+      </form>
       { loading ? <Loading/> : 
-        <select>
-          {coins.map((data,i) => (<option key={i}>{data.name} [price: {(data.quotes.USD.ath_price).toFixed(3)}]</option>))}
-        </select>
+        <ul>
+          {coins.map((data,i) => (<li key={i} className="flex__between"> 
+            <p><strong style={{fontSize:26}}>{data.name}</strong> | <span>현재 시세 : {Math.round(data.quotes.USD.ath_price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</span><br/></p>
+            <p>구매가능 : &nbsp;
+              <strong>{Math.round(moneys.replaceAll(',', '')/data.quotes.USD.ath_price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 개</strong>
+            </p>
+            </li>))}
+        </ul>
       }
     </div>
   )
